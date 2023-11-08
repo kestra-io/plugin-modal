406d8dd5567c396009d2c9b9ad1abea709ac48c2
Ludovic DEHON <tchiot.ludo@gmail.com>
2023-11-08 14:19:24 +0100

    feat(core): add NamespaceFiles props
    relate to kestra-io/kestra#2405

--- src/main/java/io/kestra/plugin/modal/cli/ModalCLI.java
+++ src/main/java/io/kestra/plugin/modal/cli/ModalCLI.java
@@ -2,6 +2,8 @@ package io.kestra.plugin.modal.cli;
 
 import io.kestra.core.models.annotations.Example;
 import io.kestra.core.models.annotations.Plugin;
+import io.kestra.core.models.tasks.NamespaceFiles;
+import io.kestra.core.models.tasks.NamespaceFilesInterface;
 import io.kestra.plugin.scripts.exec.scripts.models.DockerOptions;
 import io.kestra.plugin.scripts.exec.scripts.models.RunnerType;
 import io.kestra.plugin.scripts.exec.scripts.models.ScriptOutput;
@@ -82,7 +84,7 @@ import static io.kestra.core.utils.Rethrow.throwFunction;
         )        
     }
 )
-public class ModalCLI extends Task implements RunnableTask<ScriptOutput> {
+public class ModalCLI extends Task implements RunnableTask<ScriptOutput>, NamespaceFilesInterface {
     private static final String DEFAULT_IMAGE = "ghcr.io/kestra-io/modal";
 
     @Schema(
@@ -116,6 +118,8 @@ public class ModalCLI extends Task implements RunnableTask<ScriptOutput> {
     @Builder.Default
     protected DockerOptions docker = DockerOptions.builder().build();
 
+    private NamespaceFiles namespaceFiles;
+
     @Override
     public ScriptOutput run(RunContext runContext) throws Exception {
         return new CommandsWrapper(runContext)
@@ -123,6 +127,7 @@ public class ModalCLI extends Task implements RunnableTask<ScriptOutput> {
             .withRunnerType(RunnerType.DOCKER)
             .withDockerOptions(injectDefaults(getDocker()))
             .withEnv(Optional.ofNullable(env).orElse(new HashMap<>()))
+            .withNamespaceFiles(namespaceFiles)
             .withCommands(
                 ScriptService.scriptCommands(
                     List.of("/bin/sh", "-c"),
